b794b3a7ef682f9b7da2aaee6821246e2285872c
KimSound424 <ykj1512@gmail.com>
2023-12-25 20:15:45 +0900

    [Feature] Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/CreateAccount";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "profile",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/create-account",
+      element: <CreateAccount/>
+    }
+  ]);
+
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   );
 }
