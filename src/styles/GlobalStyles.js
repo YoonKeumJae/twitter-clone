7dacc7da00713372a2308923ade6524664407ca0
KimSound424 <ykj1512@gmail.com>
2023-12-27 16:33:22 +0900

    [ErrorFix] GlobalStyle 문제해결

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,12 +1,12 @@
 import reset from "styled-reset";
-import createGlobalStyle from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
   *{
     box-sizing: border-box;
   }
-  h1{
+  body{
     background-color: black;
     color: white;
     font-family: system-ui;
