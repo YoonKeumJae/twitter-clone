271683b6734bc53e292ee85ee52630ffa59dff61
KimSound424 <ykj1512@gmail.com>
2023-12-28 19:41:47 +0900

    [Feature] CreateAccount 로직, CSS

--- src/routes/CreateAccount.js
+++ src/routes/CreateAccount.js
@@ -1,17 +1,102 @@
 import styled from "styled-components";
+import { useState } from "react";
 
-const Wrapper = styled.div``;
-const Form = styled.form``;
-const Input = styled.input``;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0;
+`;
+const Title = styled.h1`
+  font-size: 42px;
+`;
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
 
 const CreateAccount = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [error, setError] = useState("");
+
+  const onChange = (e) => {
+    const target = e.target;
+    if (target.name === "name") {
+      setName(target.value);
+    } else if (target.name === "email") {
+      setEmail(target.value);
+    } else if (target.name === "password") {
+      setPassword(target.value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault(); // prevent refresh
+    try {
+      // create an account
+      // set the name of user's profile
+      // redirect to homepage
+    } catch (e) {
+      // setError(e.message);
+    } finally {
+      setIsLoading(false);
+    }
+    console.log(name, email, password);
+  };
+
   return (
     <Wrapper>
-      <Form>
-        <Input name="name" placeholder="name" type="text" required />
-        <Input name="email" placeholder="email" type="email" required />
-        <Input name="password" placeholder="Password" type="password" required />
-        <Input type="submit" value="Create Account" />
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="name"
+          value={name}
+          placeholder="name"
+          type="text"
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
       </Form>
     </Wrapper>
   );
