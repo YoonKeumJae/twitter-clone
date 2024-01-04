2da2a194c8c499d8af7362f54a071dfefabe7151
KimSound424 <ykj1512@gmail.com>
2024-01-04 16:49:10 +0900

    [Feature] Tweet input form & style

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
+import styled from "styled-components";
+import PostTweetForm from "../components/PostTweetForm";
 
+const Wrapper = styled.div``;
 
 const Home = () => {
-  
   return (
-    <>
-      
-    </>
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
   );
 };
 
