660ffd56bc93fa2265d9147de94b62d31f920b3a
KimSound424 <ykj1512@gmail.com>
2024-01-04 15:41:09 +0900

    [Fix] Logout test btn 삭제

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,10 @@
-import { auth } from "../firebase";
+
 
 const Home = () => {
-  const logOut = () => {
-    auth.signOut();
-  };
+  
   return (
     <>
-      <h1>
-        <button onClick={logOut}>Logout</button>
-      </h1>
+      
     </>
   );
 };
